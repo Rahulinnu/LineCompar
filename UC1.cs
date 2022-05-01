43d11a71689525e86b53f2730457b1b77e3ecc63
rahul unni <rahul@rahulsmac.local>
2022-05-02 00:29:03 +0530

    UC1 added

---
+++ UC1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LineComparison
+{
+    public class UC1
+    {
+        public static void Usca1(int x1, int y1, int x2, int y2)
+        {
+            double result, sum;
+            sum = ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            result = Math.Sqrt(sum);
+            Console.WriteLine("The length is :" + result);
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
