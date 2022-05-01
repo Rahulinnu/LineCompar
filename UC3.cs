9b0d34b4a4da1200453fc78bc6971ad58c28f54a
rahul unni <rahul@rahulsmac.local>
2022-05-02 00:32:43 +0530

    Updated

--- UC3.cs
+++ UC3.cs
@@ -3,8 +3,44 @@ namespace LineComparison
 {
     public class UC3
     {
-        public UC3()
+        public static void Usca3(int l1, int m1, int l2, int m2)
         {
+            int le1, le2;
+            if (l1 == l2)
+            {
+                int comp = m1.CompareTo(m2);
+                if (comp == 0)
+                {
+                    Console.WriteLine("Both line are equals");
+                }
+                else if (comp >= 0)
+                {
+                    Console.WriteLine("Second line is Smaller than first");
+                }
+                else
+                {
+                    Console.WriteLine("Second line is bigger than First");
+                }
+            }
+            else
+            {
+                le1 = m1 - l1;
+                le2 = m2 - l2;
+                int comp = le1.CompareTo(le2);
+                if (comp == 0)
+                {
+                    Console.WriteLine("Both line are equals");
+                }
+                else if (comp >= 0)
+                {
+                    Console.WriteLine("Second line is Smaller than first");
+                }
+                else
+                {
+                    Console.WriteLine("Second line is bigger than First");
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
