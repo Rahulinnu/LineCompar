dd67e7e170c7af14ff08d905beea81d193d4333d
rahul unni <rahul@rahulsmac.local>
2022-05-05 15:26:17 +0530

    UC2 updated

--- UC2.cs
+++ UC2.cs
@@ -3,8 +3,20 @@ namespace LineComparison
 {
     public class UC2
     {
-        public UC2()
+        public static void Usca2(int a1, int b1, int a2, int b2)
         {
+            int len1, len2;
+            if (a1 == a2)
+            {
+                Console.WriteLine(b1.Equals(b2));
+            }
+            else
+            {
+                len1 = b1 - a1;
+                len2 = b2 - a2;
+                Console.WriteLine(len1.Equals(len2));
+                Console.ReadLine();
+            }
         }
     }
 }
